354fe4e4a8167a7b24094b4d11fa347c191a44f7
jziemian <jaznet.solutions@gmail.com>
2024-03-06 17:39:46 -0500

    ok

--- jz.studio.client/src/library/jz-docking/jz-docking/jz-docking.component.ts
+++ jz.studio.client/src/library/jz-docking/jz-docking/jz-docking.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DockManager } from "../DockSpawn/lib/js/DockManager";
+import { PanelContainer } from "../DockSpawn/lib/js/PanelContainer";
+import { PanelType } from "../DockSpawn/lib/js/enums/PanelType";
 
 @Component({
   selector: 'app-jz-docking',
