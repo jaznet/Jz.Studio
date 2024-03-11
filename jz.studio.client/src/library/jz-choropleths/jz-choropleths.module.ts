069c4eef6312b5fc68a062145b49ff1e281a48b4
jziemian <jaznet.solutions@gmail.com>
2024-03-11 12:07:56 -0400

    ok

--- jz.studio.client/src/library/jz-choropleths/jz-choropleths.module.ts
+++ jz.studio.client/src/library/jz-choropleths/jz-choropleths.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ChoroUsaComponent } from './components/choro-usa/choro-usa.component';
 import { ChoroStateComponent } from './components/choro-state/choro-state.component';
 import { CountyPaintingStrategy } from './interface/county-painting-strategy';
-import { ChoroDashComponent } from './components/choro-dash/choro-dash.component';
 import { JzChoroplethsRouterModule } from './jz-choropleths-router.module'
 import { PaintStrategyFactoryService } from '../jz-chorodash/paint-factory/paint-strategy-factory.service';
 
@@ -13,7 +12,7 @@ export const PAINTING_STRATEGY_TOKEN = new InjectionToken<CountyPaintingStrategy
   declarations: [
     ChoroUsaComponent,
     ChoroStateComponent,
-    ChoroDashComponent
+    
   ],
   imports: [
     CommonModule,
@@ -30,7 +29,7 @@ export const PAINTING_STRATEGY_TOKEN = new InjectionToken<CountyPaintingStrategy
   exports: [
     ChoroUsaComponent,
     ChoroStateComponent,
-    ChoroDashComponent
+    
   ]
 })
 export class JzChoroplethsModule { }
