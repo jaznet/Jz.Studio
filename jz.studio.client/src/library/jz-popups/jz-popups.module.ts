20bba09c5fe19a9258c579bd836c2749df0c38f8
jziemian <jaznet.solutions@gmail.com>
2024-03-18 12:19:19 -0400

    ok

--- jz.studio.client/src/library/jz-popups/jz-popups.module.ts
+++ jz.studio.client/src/library/jz-popups/jz-popups.module.ts
@@ -5,12 +5,14 @@ import { PopoverBaseComponent } from './popover-base/popover-base.component';
 import { PopoverHttpErrorComponent } from '../jz-popups/popover-http-error/popover-http-error.component'
 import { PopoverLoadingComponent } from '../jz-popups/popover-loading/popover-loading.component'
 import { JzUiControlsModule } from '../jz-ui-controls/jz-ui-controls.module';
+import { PopUpLoadingComponent } from './pop-up-loading/pop-up-loading.component';
 
 @NgModule({
   declarations: [
     PopoverLoadingComponent,
     PopoverHttpErrorComponent,
-    PopoverBaseComponent
+    PopoverBaseComponent,
+    PopUpLoadingComponent
   ],
   imports: [
     CommonModule,
---
+++ jz.studio.client/src/library/jz-popups/pop-up-loading/pop-up-loading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pop-up-loading',
+  templateUrl: './pop-up-loading.component.html',
+  styleUrl: './pop-up-loading.component.css'
+})
+export class PopUpLoadingComponent {
+
+}
