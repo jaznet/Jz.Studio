f09fc76e533f1fb8a04762df1e6a3ad7897570ec
jziemian <jaznet.solutions@gmail.com>
2024-03-20 15:29:08 -0400

    ok

--- jz.studio.client/src/library/jz-choro-dash/jz-choro-dash.component.ts
+++ jz.studio.client/src/library/jz-choro-dash/jz-choro-dash.component.ts
@@ -1,16 +1,21 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'jz-choro-dash',
   templateUrl: './jz-choro-dash.component.html',
   styleUrl: './jz-choro-dash.component.css'
 })
-export class JzChoroDashComponent implements OnInit {
+export class JzChoroDashComponent implements OnInit, AfterViewInit {
   @HostBinding('class') classes = 'fit-to-parent';
 
-  constructor() { }
+  constructor(private element: ElementRef) {
+    console.log(this.element.nativeElement.clientWidth, this.element.nativeElement.ClientHeight);
+  }
+    ngAfterViewInit(): void {
+      console.log(this.element.nativeElement.clientWidth, this.element.nativeElement.ClientHeight);
+    }
 
     ngOnInit(): void {
-       
+      console.log(this.element.nativeElement.clientWidth, this.element.nativeElement.ClientHeight);
     }
 }
