07815466f5f1301ccda9b75539c6b97a9ab0b955
jziemian <jaznet.solutions@gmail.com>
2024-03-20 09:30:19 -0400

    pop over

--- jz.studio.client/src/app-view-modules/sandbox/sandbox.component.ts
+++ jz.studio.client/src/app-view-modules/sandbox/sandbox.component.ts
@@ -1,18 +1,25 @@
-import { AfterViewInit, Component, HostBinding, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, HostBinding, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { PopOverLoadingComponent } from '../../library/jz-popups/pop-over-loading/pop-over-loading.component';
 import { DynamicPopoverComponent } from '../../library/jz-popups/dynamic-popover/dynamic-popover.component';
+import { JzPopupsService } from '../../library/jz-popups/jz-popups.service';
 
 @Component({
   selector: 'app-sandbox',
   templateUrl: './sandbox.component.html',
   styleUrls: ['./sandbox.component.css']
 })
-export class SandboxComponent implements AfterViewInit {
-
+export class SandboxComponent implements OnInit, AfterViewInit {
   @HostBinding('class') classes = 'fit-to-parent ';
   @ViewChild('popOverLoadingComponent') popover!: PopOverLoadingComponent
 
-  contructor() { }
+  constructor(private popoverservice:JzPopupsService) { }
+
+  ngOnInit(): void {
+    this.popoverservice.popoverEvent.subscribe((event: any) => {
+      this.popover.isPopupVisible = true;
+      console.log(event);
+    })
+  }
 
   ngAfterViewInit(): void {
     console.log(this.popover.target);
--- jz.studio.client/src/library/jz-popups/jz-popups.service.ts
+++ jz.studio.client/src/library/jz-popups/jz-popups.service.ts
@@ -4,14 +4,9 @@ import { EventEmitter, Injectable, Output } from '@angular/core';
   providedIn: 'root'
 })
 export class JzPopupsService {
-  @Output() popUpEvent = new EventEmitter();
   @Output() popoverEvent = new EventEmitter();
 
   constructor() { }
-
-  showPopupLoading() {
-    /*this.popUpEvent.emit();*/
-  }
 
   showPopoverLoading() {
     this.popoverEvent.emit();
