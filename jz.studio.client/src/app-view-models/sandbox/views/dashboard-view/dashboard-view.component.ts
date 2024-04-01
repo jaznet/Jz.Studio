5ba4d89c184c744df3bbe3ac0be6b9b9df033bb4
jziemian <jaznet.solutions@gmail.com>
2024-04-01 08:11:53 -0400

    syncfusion dashboard

--- jz.studio.client/src/app-view-models/sandbox/views/dashboard-view/dashboard-view.component.ts
+++ jz.studio.client/src/app-view-models/sandbox/views/dashboard-view/dashboard-view.component.ts
@@ -1,17 +1,28 @@
 
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, HostBinding, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard-view',
+  selector: 'dashboard-view',
   templateUrl: './dashboard-view.component.html',
   styleUrl: './dashboard-view.component.css'
 })
 export class DashboardViewComponent implements OnInit, AfterViewInit {
-  cellSpacing = 4;
+  @HostBinding('class') classes = 'fit-to-parent ';
+
+  cellSpacing : number[] = [10, 10];
+  public panels: any = [{ "sizeX": 1, "sizeY": 1, "row": 0, "col": 0, content: '<div class="content">0</div>' },
+  { "sizeX": 3, "sizeY": 2, "row": 0, "col": 1, content: '<div class="content">1</div>' },
+  { "sizeX": 1, "sizeY": 3, "row": 0, "col": 4, content: '<div class="content">2</div>' },
+  { "sizeX": 1, "sizeY": 1, "row": 1, "col": 0, content: '<div class="content">3</div>' },
+  { "sizeX": 2, "sizeY": 1, "row": 2, "col": 0, content: '<div class="content">4</div>' },
+  { "sizeX": 1, "sizeY": 1, "row": 2, "col": 2, content: '<div class="content">5</div>' },
+  { "sizeX": 1, "sizeY": 1, "row": 2, "col": 3, content: '<div class="content">6</div>' }
+  ]
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
 
   ngAfterViewInit(): void {
     let element = document.getElementById("app-body");
@@ -23,3 +34,4 @@ export class DashboardViewComponent implements OnInit, AfterViewInit {
   }
 
 }
+
