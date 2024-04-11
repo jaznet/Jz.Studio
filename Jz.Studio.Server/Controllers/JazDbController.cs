202f133f316034243e802e722a5ab1d386984d96
jziemian <jaznet.solutions@gmail.com>
2024-04-11 10:24:18 -0400

    ok

--- Jz.Studio.Server/Controllers/JazDbController.cs
+++ Jz.Studio.Server/Controllers/JazDbController.cs
@@ -1,8 +1,11 @@
-﻿using Jz.Studio.Server.Data.JazDb;
+﻿
+//using Jz.Studio.Server.Data.JazDb;
+using Jz.Studio.Server.Data.JazDb;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Jz.Studio.Server.Controllers {
+
     [Route("api/[controller]")]
     [ApiController]
     public class JazDbController : ControllerBase {
@@ -12,13 +15,13 @@ namespace Jz.Studio.Server.Controllers {
             _context = context;
         }
 
-        [HttpGet("election")]
-        public async Task<ActionResult<IEnumerable<Election2020>>> GetElectionResults() {
-            if (_context == null) {
-                return NotFound();
-            }
-            Console.WriteLine("GetElectionResults");
-            return await _context.Election2020s.ToListAsync();
-        }
+        //[HttpGet("election")]
+        //public async Task<ActionResult<IEnumerable<ElectionDatum>>> GetElectionResults() {
+        //    if (_context == null) {
+        //        return NotFound();
+        //    }
+        //    Console.WriteLine("GetElectionResults");
+        //    return await _context.ElectionDatum.ToListAsync();
+        //}
     }
 }
---
+++ Jz.Studio.Server/Data/JazDb/ElectionDatum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jz.Studio.Server.Data.JazDb;
+
+public partial class ElectionDatum
+{
+    public string? Year { get; set; }
+
+    public string? State { get; set; }
+
+    public string? StatePo { get; set; }
+
+    public string? StateFips { get; set; }
+
+    public string? StateCen { get; set; }
+
+    public string? StateIc { get; set; }
+
+    public string? Office { get; set; }
+
+    public string? Candidate { get; set; }
+
+    public string? PartyDetailed { get; set; }
+
+    public string? Writein { get; set; }
+
+    public string? Candidatevotes { get; set; }
+
+    public string? Totalvotes { get; set; }
+
+    public string? Version { get; set; }
+
+    public string? Notes { get; set; }
+
+    public string? PartySimplified { get; set; }
+}
