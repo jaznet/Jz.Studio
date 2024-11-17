6eab67a87fa5ec30bfe5eb549076c8c729b042c7
jaznet <jaznet.solutions@gmail.com>
2024-11-17 16:45:12 -0500

    ok

--- jz.studio.client/src/library/jz-charts/jz-technical-analysis/techanTs/techanTs.component.ts
+++ jz.studio.client/src/library/jz-charts/jz-technical-analysis/techanTs/techanTs.component.ts
@@ -1,5 +1,8 @@
 import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostBinding, OnInit, ViewChild } from '@angular/core';
 import { range } from 'rxjs';
+/*import * as d3 from 'd3';*/
+import { } from 'd3-axis';
+import { scaleTime, scaleUtc, scaleLinear } from 'd3-scale';
 import { StockPriceHistory } from '../../../../models/stock-price-history.model';
 import { JzPopOversService } from '../../../jz-pop-overs/jz-pop-overs.service';
 import { TechanTsService } from './techanTs.service';
@@ -29,9 +32,6 @@ export class TechanTsComponent implements OnInit, AfterViewInit {
   xScale!: any;
   yScale!: any;
 
-  //private techan: any;
-  //private d3v4: any;
-
   constructor(
     private changeDetector: ChangeDetectorRef,
     private stockPriceService: TechanTsService,
@@ -54,29 +54,13 @@ export class TechanTsComponent implements OnInit, AfterViewInit {
     );
   }
 
-  //private async initializeTechan(): Promise<void> {
-  //  if (!this.techan) {
-  //    try {
-  //      this.d3v4 = await import(/* webpackChunkName: "d3v4" */ 'd3-v4');
-  //      const d3CommonJS = { ...this.d3v4, __esModule: false };  // Simulate CommonJS module
-
-  //      const techanModule = await import(/* webpackChunkName: "techan" */ 'techan');
-  //      this.techan = techanModule.default(d3CommonJS); // Initialize Techan with d3 v4
-
-  //      console.log("Techan initialized successfully:", this.techan);
-  //    } catch (error) {
-  //      console.error("Failed to initialize Techan with D3:", error);
-  //    }
-  //  }
-  //}
-
   createChart(): void {
   /*  this.svg = this.d3v4.select(this.svgElementRef.nativeElement);*/
     this.svgWidth = this.svgElementRef.nativeElement.clientWidth;
     this.svgHeight = this.svgElementRef.nativeElement.clientHeight;
 
-    //this.xScale = this.d3v4.scaleUtc().domain([new Date("2023-01-01"), new Date("2024-01-01")]).range([0, 800]);
-    //this.yScale = this.d3v4.scaleLinear().domain([0, 100]).range([0, 400]);
+    this.xScale = scaleUtc().domain([new Date("2023-01-01"), new Date("2024-01-01")]).range([0, 800]);
+    this.yScale = scaleLinear().domain([0, 100]).range([0, 400]);
 
     //const candlestickPlot = this.techan.plot.candlestick().xScale(this.xScale).yScale(this.yScale);
     //this.svg.append("g").attr("class", "candlestick").call(candlestickPlot);
