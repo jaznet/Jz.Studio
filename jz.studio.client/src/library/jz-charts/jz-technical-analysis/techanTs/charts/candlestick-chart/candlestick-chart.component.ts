2473d3bf4e9715935ea8248847803cb6f8cc94dc
jaznet <53183685+jaznet@users.noreply.github.com>
2024-12-14 16:15:18 -0500

    ok

--- jz.studio.client/src/library/jz-charts/jz-technical-analysis/techanTs/charts/candlestick-chart/candlestick-chart.component.ts
+++ jz.studio.client/src/library/jz-charts/jz-technical-analysis/techanTs/charts/candlestick-chart/candlestick-chart.component.ts
@@ -1,40 +1,73 @@
-// candlestick-chart.component.ts
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-/*import * as d3 from 'd3';*/
-import { max, min, extent } from 'd3-array';
-import { scaleTime, scaleLinear } from 'd3-scale';
+import { Component, Input, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+import * as d3 from 'd3';
+import { CandlestickData } from '../../interfaces/techan-interfaces';
 
 @Component({
   selector: 'candlestick-chart',
   templateUrl: './candlestick-chart.component.html',
   styleUrls: ['./candlestick-chart.component.css']
 })
-export class CandlestickChartComponent implements OnInit {
-  @ViewChild('chart', { static: true }) private chartContainer!: ElementRef;
+export class CandlestickChartComponent implements AfterViewInit {
+  @Input() data: CandlestickData[] = []; // Input to accept candlestick data
+  @ViewChild('chartContainer', { static: true }) chartContainer!: ElementRef<HTMLElement>;
 
-  constructor() { }
+  private svg: any;
+  private xScale: any;
+  private yScale: any;
+  private chartWidth = 800;
+  private chartHeight = 400;
 
-  ngOnInit(): void {
-    this.createChart();
+  ngAfterViewInit(): void {
+    this.initializeChart();
+    this.drawChart();
   }
 
-  private createChart(): void {
-    const element = this.chartContainer.nativeElement;
-    const data = this.getData();
+  private initializeChart(): void {
+    // Initialize SVG
+    this.svg = d3.select(this.chartContainer.nativeElement)
+      .append('svg')
+      .attr('width', this.chartWidth)
+      .attr('height', this.chartHeight);
 
-    //const candlestickXScale = scaleTime()
-    //  .domain(extent(data, d => d.date) as [Date, Date])
-    //  .range([0, element.offsetWidth]);
+    // Initialize scales
+    this.xScale = d3.scaleBand<Date>()
+      .domain(this.data.map(d => d.date))
+      .range([0, this.chartWidth])
+      .padding(0.1);
 
-    //const candlestickYScale = scaleLinear()
-    //  .domain([0, max(data, d => d.high) as number])
-    //  .range([element.offsetHeight, 0]);
+    const priceValues = this.data.flatMap(d => [d.open, d.high, d.low, d.close]);
+    const minPrice = d3.min(priceValues) ?? 0;
+    const maxPrice = d3.max(priceValues) ?? 100;
 
-    // Render candlestick chart using xScale and yScale
+    this.yScale = d3.scaleLinear()
+      .domain([minPrice, maxPrice])
+      .range([this.chartHeight, 0]);
   }
 
-  private getData() {
-    // Fetch or generate data
-    return [];
+  private drawChart(): void {
+    // Draw candlestick bodies
+    const candle = this.svg.selectAll('.candle')
+      .data(this.data)
+      .enter()
+      .append('rect')
+      .attr('class', 'candle')
+      .attr('x', (d: CandlestickData) => this.xScale(d.date) ?? 0)
+      .attr('y', (d: CandlestickData) => this.yScale(Math.max(d.open, d.close)))
+      .attr('width', this.xScale.bandwidth())
+      .attr('height', (d: CandlestickData) => Math.abs(this.yScale(d.open) - this.yScale(d.close)))
+      .attr('fill', (d: CandlestickData) => d.open > d.close ? '#bf211e' : 'seagreen');
+
+    // Draw candlestick wicks
+    const wicks = this.svg.selectAll('.wick')
+      .data(this.data)
+      .enter()
+      .append('line')
+      .attr('class', 'wick')
+      .attr('x1', (d: CandlestickData) => (this.xScale(d.date) ?? 0) + this.xScale.bandwidth() / 2)
+      .attr('x2', (d: CandlestickData) => (this.xScale(d.date) ?? 0) + this.xScale.bandwidth() / 2)
+      .attr('y1', (d: CandlestickData) => this.yScale(d.high))
+      .attr('y2', (d: CandlestickData) => this.yScale(d.low))
+      .attr('stroke', '#52aa8a')
+      .attr('stroke-width', 1);
   }
 }
