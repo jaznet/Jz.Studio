7d7c2b28750fc77abe0828178830c1d6b573dd64
jaznet <53183685+jaznet@users.noreply.github.com>
2025-01-15 11:35:35 -0500

    ok

--- jz.studio.client/src/library/jz-charts/jz-technical-analysis/techanTs/charts/charts.module.ts
+++ jz.studio.client/src/library/jz-charts/jz-technical-analysis/techanTs/charts/charts.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { VolumeChartComponent } from './volume-chart/volume-chart.component';
 import { CandlestickChartComponent } from './candlestick-chart/candlestick-chart.component';
 
 @NgModule({
