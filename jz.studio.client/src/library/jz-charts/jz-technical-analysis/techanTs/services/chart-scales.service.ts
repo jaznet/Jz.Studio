34f1144fe98db1e0b4fb1e83e6f53ac5bad3dbf1
jaznet <53183685+jaznet@users.noreply.github.com>
2025-03-08 15:46:35 -0500

    ok

--- jz.studio.client/src/library/jz-charts/jz-technical-analysis/techanTs/services/chart-scales.service.ts
+++ jz.studio.client/src/library/jz-charts/jz-technical-analysis/techanTs/services/chart-scales.service.ts
@@ -35,7 +35,7 @@ export class ChartScalesService {
 
     this.ohlcYscale = scaleLinear()
       .domain([this.data.minPrice ?? 0, this.data.maxPrice ?? 100]) // Using minPrice and maxPrice to define the domain
-      .range([this.layout.sectionRectA.height.baseVal.value, 0]); // Invert the range for correct orientation (top to bottom)
+      .range([this.layout.sectionRectA.height.baseVal.value* .75, 0]); // Invert the range for correct orientation (top to bottom)
 
     this.volumeYscale = scaleLinear()
       .domain([0, this.data.maxVolume ?? 10000000]) // Using minPrice and maxPrice to define the domain
