34285325253531f9e192320cdb7e7842b7df069f
jaznet <53183685+jaznet@users.noreply.github.com>
2025-05-06 07:50:33 -0400

    ok

--- jz.studio.client/src/library/jz-charts/jz-technical-analysis/techanTs/services/layout.service.ts
+++ jz.studio.client/src/library/jz-charts/jz-technical-analysis/techanTs/services/layout.service.ts
@@ -237,6 +237,6 @@ export class LayoutService {
     this.rsiLayout.gSection.attr('transform', `translate( 0,  ${(this.scaffold.sections[0].height + this.scaffold.sections[1].height + this.scaffold.sections[2].height) + (this.spacer * 4)})`);
     this.rsiLayout.gAxisGroupRight.attr('transform', `translate(${this.scaffold.sections[3].width - this.scaffold.sections[3].margins.right},${this.scaffold.sections[3].margins.top})`);
     this.rsiLayout.gAxisLeft.attr('transform', `translate(${this.scaffold.sections[3].margins.left},0)`);
-/*    this.rsiLayout.gSection.attr('transform', `translate(${this.scaffold.sections[3].margins.left},0)`);*/
+    this.rsiLayout.gContent.attr('transform', `translate(${this.scaffold.sections[3].margins.left},0)`);
   }
 }
