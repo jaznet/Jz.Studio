1f57d838ec0e1f09282cb0d5226dd06ab6715518
jaznet <53183685+jaznet@users.noreply.github.com>
2025-09-04 15:01:33 -0400

    ok

--- jz.studio.client/src/library/jz-charts/techanTs/components/chart-components.module.ts
+++ jz.studio.client/src/library/jz-charts/techanTs/components/chart-components.module.ts
@@ -2,9 +2,10 @@ import { NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MacdChartComp } from './macd-chart/macd-chart.component';
 import { OhlcChartComponent } from './ohlc-chart/ohlc-chart.component';
+import { VolumeChartComponent } from './volume-chart/volume-chart.component';
 
 @NgModule({
-  declarations: [MacdChartComp, OhlcChartComponent],
+  declarations: [MacdChartComp, OhlcChartComponent, VolumeChartComponent],
   schemas: [NO_ERRORS_SCHEMA],
   exports: [
     MacdChartComp,
---
+++ jz.studio.client/src/library/jz-charts/techanTs/components/volume-chart/volume-chart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VolumeChartComponent } from './volume-chart.component';
+
+describe('VolumeChartComponent', () => {
+  let component: VolumeChartComponent;
+  let fixture: ComponentFixture<VolumeChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [VolumeChartComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(VolumeChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
