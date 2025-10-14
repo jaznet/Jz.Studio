0f5ad021f7922312ebc2ba367f7c76ee23f2f573
jaznet <53183685+jaznet@users.noreply.github.com>
2025-10-14 15:57:30 -0400

    ok

--- jz.studio.client/src/app/app.module.ts
+++ jz.studio.client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 // app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'; // standalone
@@ -13,20 +13,13 @@ import { SandboxModule } from '../app-view-models/sandbox/sandbox.module';
 import { DatavizModule } from '../app-view-models/dataviz/dataviz.module';
 import { GraphicsModule } from '../app-view-models/graphics/graphics.module';
 
-@NgModule({
-  // declarations: []     // keep empty – don't declare a standalone component
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,     // handles RouterModule.forRoot(...)
-    AppComponent,         // <-- add the standalone root here
-    JzUiControlsModule,
-    AppPartsModule,
-    AppWelcomeModule,
-    SandboxModule,
-    GraphicsModule,
-    DatavizModule
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule, // handles RouterModule.forRoot(...)
+        AppComponent, // <-- add the standalone root here
+        JzUiControlsModule,
+        AppPartsModule,
+        AppWelcomeModule,
+        SandboxModule,
+        GraphicsModule,
+        DatavizModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
