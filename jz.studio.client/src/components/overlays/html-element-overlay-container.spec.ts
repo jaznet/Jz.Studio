a42fe9c10132d7be5226e29fd0f153185022d910
jaznet <53183685+jaznet@users.noreply.github.com>
2025-10-26 17:02:54 -0400

    ok

---
+++ jz.studio.client/src/components/overlays/html-element-overlay-container.spec.ts
@@ -0,0 +1,7 @@
+import { HtmlElementOverlayContainer } from './html-element-overlay-container';
+
+describe('HtmlElementOverlayContainer', () => {
+  it('should create an instance', () => {
+    expect(new HtmlElementOverlayContainer()).toBeTruthy();
+  });
+});
