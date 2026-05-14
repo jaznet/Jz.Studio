ffb90b98b837c2699d53e085c1f86367dc2df21f
jaznet <53183685+jaznet@users.noreply.github.com>
2026-05-14 08:20:39 -0400

    ok

--- jz.studio.client/src/_apps/visualization/visualization.routes.ts
+++ jz.studio.client/src/_apps/visualization/visualization.routes.ts
@@ -1,11 +1,5 @@
 import { Routes } from '@angular/router';
-import { JzChoroDashComponent } from '../../_components/jz-choro-dash/jz-choro-dash.component';
 import { VisualizationComponent } from './visualization.component';
-import { VisualizationHomeComponent } from './components/visualization-home/visualization-home.component';
-import { TechnicalAnalysisComponent } from '../../_components/charts/technical-analysis/technical-analysis.component';
-import { SankeyComponent } from '../../_components/charts/jz-sankey/jz-sankey.component';
-import { JzBubbleChart } from '../../_components/charts/jz-bubble-chart/jz-bubble-chart';
-import { JzSyncfusionChartComponent } from '../../_components/charts/jz-syncfusion-chart/jz-syncfusion-chart.component';
 
 export const VISUALIZATION_ROUTES: Routes = [
   {
