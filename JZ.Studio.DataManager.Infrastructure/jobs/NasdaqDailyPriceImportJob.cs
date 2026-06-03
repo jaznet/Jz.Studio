a781994caef0925bbe4ccdec698b77a0ce067659
jaznet <53183685+jaznet@users.noreply.github.com>
2026-06-03 18:42:29 -0400

    ok

---
+++ JZ.Studio.DataManager.Infrastructure/jobs/NasdaqDailyPriceImportJob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace JZ.Studio.DataManager.Infrastructure.jobs;
+
+public class NasdaqDailyPriceImportJob {
+	public void Execute(string filePath) {
+		Console.WriteLine($"Reading: {filePath}");
+
+		if (!File.Exists(filePath)) {
+			Console.WriteLine("File not found.");
+			return;
+		}
+
+		var lines = File.ReadLines(filePath).Take(10);
+
+		foreach (var line in lines) {
+			Console.WriteLine(line);
+		}
+	}
+}
--- JZ.Studio.DataManager.Worker/Worker.cs
+++ JZ.Studio.DataManager.Worker/Worker.cs
@@ -1,4 +1,5 @@
 using JZ.Studio.DataManager.Core.contracts;
+using JZ.Studio.DataManager.Infrastructure.jobs;
 namespace JZ.Studio.DataManager.Worker;
 public class Worker : BackgroundService {
 	private readonly ILogger<Worker> _logger;
@@ -9,15 +10,25 @@ public class Worker : BackgroundService {
 		_logger = logger;
 		_serviceProvider = serviceProvider;
 	}
-	protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
-		_logger.LogInformation("JZ Studio DataManager Worker started at: {time}", DateTimeOffset.Now);
-		using var scope = _serviceProvider.CreateScope();
-		var jobs = scope.ServiceProvider.GetServices<IEtlJob>();
-		foreach (var job in jobs) {
-			_logger.LogInformation("Starting ETL job: {JobName}", job.Name);
-			await job.RunAsync(stoppingToken);
-			_logger.LogInformation("Finished ETL job: {JobName}", job.Name);
-		}
-		_logger.LogInformation("JZ Studio DataManager Worker finished initial ETL run at: {time}", DateTimeOffset.Now);
+	//protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+	//	_logger.LogInformation("JZ Studio DataManager Worker started at: {time}", DateTimeOffset.Now);
+	//	using var scope = _serviceProvider.CreateScope();
+	//	var jobs = scope.ServiceProvider.GetServices<IEtlJob>();
+	//	foreach (var job in jobs) {
+	//		_logger.LogInformation("Starting ETL job: {JobName}", job.Name);
+	//		await job.RunAsync(stoppingToken);
+	//		_logger.LogInformation("Finished ETL job: {JobName}", job.Name);
+	//	}
+	//	_logger.LogInformation("JZ Studio DataManager Worker finished initial ETL run at: {time}", DateTimeOffset.Now);
+	//}
+
+	protected override async Task ExecuteAsync(
+	CancellationToken stoppingToken) {
+		var job = new NasdaqDailyPriceImportJob();
+
+		job.Execute(
+			@"C:\MarketData\NASDAQ_20260106.txt");
+
+		await Task.CompletedTask;
 	}
 }
