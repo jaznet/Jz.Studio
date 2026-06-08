341bdc3ef8216643da5c830889fc34fcd30730d8
jaznet <53183685+jaznet@users.noreply.github.com>
2026-06-08 08:51:02 -0400

    ok

--- jz.studio.client/src/_framework/ui/popovers/jz-popover-loading/jz-popover-loading.component.ts
+++ jz.studio.client/src/_framework/ui/popovers/jz-popover-loading/jz-popover-loading.component.ts
@@ -1,18 +1,16 @@
-import { Component, Inject } from '@angular/core';
-import { JzPopoverBaseComponent } from '../jz-popover-base/jz-popover-base.component';
-import { JZ_POPOVER_DATA } from '../jz-popover-injector.tokens';
-import { JzPopoverLoadingData } from '../models/jz-popover-loading-data';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { JzPopoverPanelComponent } from '../jz-popover-panel/jz-popover-panel.component';
 
 @Component({
   selector: 'jz-popover-loading',
   standalone: true,
-  imports: [JzPopoverBaseComponent],
+  imports: [
+    CommonModule,
+    JzPopoverPanelComponent
+  ],
   templateUrl: './jz-popover-loading.component.html',
-  styleUrl: './jz-popover-loading.component.scss'
+  styleUrls: ['./jz-popover-loading.component.scss']
 })
 export class JzPopoverLoadingComponent {
-  constructor(
-    @Inject(JZ_POPOVER_DATA)
-    public readonly data: JzPopoverLoadingData
-  ) { }
 }
--- jz.studio.client/src/_framework/ui/popovers/jz-popover-panel/jz-popover-panel.component.ts
+++ jz.studio.client/src/_framework/ui/popovers/jz-popover-panel/jz-popover-panel.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+export type JzPopoverPanelVariant =
+  | 'default'
+  | 'loading'
+  | 'info'
+  | 'success'
+  | 'warning'
+  | 'error';
 
 @Component({
   selector: 'jz-popover-panel',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './jz-popover-panel.component.html',
   styleUrls: ['./jz-popover-panel.component.scss']
 })
 export class JzPopoverPanelComponent {
+
+  @Input()
+  title = '';
+
+  @Input()
+  icon = '';
+
+  @Input()
+  variant: JzPopoverPanelVariant = 'default';
+
+  @Input()
+  showFooter = false;
+
 }
