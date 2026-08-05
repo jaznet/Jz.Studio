92f568be9c788232d9a680aa88369992ec934860
John Ziemian <53183685+jaznet@users.noreply.github.com>
2026-08-05 08:44:29 -0400

    Add TopoJSON models to ChoroDash library

--- jz.studio.client/libraries/apps/jz-choro-dash/src/public-api.ts
+++ jz.studio.client/libraries/apps/jz-choro-dash/src/public-api.ts
@@ -10,6 +10,7 @@ export * from './lib/models/county-selection.model';
 export * from './lib/models/federal-election';
 export * from './lib/models/geo-shape-set.model';
 export * from './lib/models/population';
+export * from './lib/models/my-topo-json.model';
 
 export * from './lib/paint-factory/interfaces/county-painting-strategy';
 export * from './lib/paint-factory/paint-strategy-factory.service';
